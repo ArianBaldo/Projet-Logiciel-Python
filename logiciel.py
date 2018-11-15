bce36309e63064550457e561f3743ef20f0b77fa
1possible <emvbogae@ulb.ac.be>
2018-11-16 00:48:38 +0100

    Update logiciel.py

--- logiciel.py
+++ logiciel.py
@@ -14,8 +14,8 @@ import math
 """
             #ptot = pression actuelle totale. Autrement dit la pression ambiante dans le lieu ou se trouve le séchoir.
             #Ta = Temperature de l'air ambiante (en Kelvin)
-            #Ma = Masse molaire de l'air (sec)
-            #Me = Masse molaire de l'eau
+Ma=0.8*28+0.2*32         #Ma = Masse molaire de l'air (sec)
+Me= 18.016         #Me = Masse molaire de l'eau
             #t = temps exprimé en heures de séchage
 
             #Trose = température de rosée
@@ -25,30 +25,35 @@ import math
             #Ha est l'humidité absolue
             #Fd est le flux direct (W/m**2)
             #Fi Indirect est le flux indirect (W/m**2)
+            #Pe pression partiel de la vapeur
 
             #tempSerre la temperature de la serre (K)
 
             #Q est le debit d'air(m**3/s)
 
             #puissance e
-h=0         #h est le coefficient d'échange de chaleur entre la surface et le toit
+            #h est le coefficient d'échange de chaleur entre la surface et le toit
             #Fs est le flux d'énérgie produit par le sols par la loie des corps noirs
             #Ft est le flux d'énérgie produit par le toit par la loie des corps noirs
             #tempSurf est la temperature de la surface
             #tempToit est la temperature du toit
+
 #####################################################################
-"""
-#Block Environnement
-"""
-Trose =
 
-Hr = 
+def BlockEnvironnement(Pto,Ta,t,ptot):
+    psat =
+    Fd=
+    Hr =Pe/psat*100
 
-psat =
+    Trose = 373.15/(1-math.log(101325/ptot,math.e))
 
-Tsky = Ta*(0.711 + (0.005 * Trosé) + (7,3 * (10 ** -5) * (Trosé ** 2)) + 0.013 * math.cos((2*pi*t)/24))**1/4
+    Tsky = Ta*(0.711 + (0.005 * Trose) + (7,3 * (10 ** -5) * (Trose ** 2)) + 0.013 * math.cos((2*math.pi*t)/24))**1/4
 
-Ha = (Me/Ma) * ((Hr * psat)/(ptot-(Hr*psat)))
+    Ha = (Me/Ma) * ((Hr * psat)/(ptot-(Hr*psat)))
+
+    Fi = 5.67*10**(-8)*Tsky
+
+    return Fi,Ha,Fd
 
 
 
